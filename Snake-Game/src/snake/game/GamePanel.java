9a86d05d89dfaee00f778a6d07c63dc74cd028eb
Phoenix-Firas <106271549+Phoenix-Firas@users.noreply.github.com>
2022-11-13 19:43:46 +0200

    implemented newApple method functionality

--- Snake-Game/src/snake/game/GamePanel.java
+++ Snake-Game/src/snake/game/GamePanel.java
@@ -60,6 +60,8 @@ public class GamePanel extends JPanel implements ActionListener{
     }
     
     public void newApple(){
+        appleX = random.nextInt((int)(SCREEN_WIDTH / UNIT_SIZE))* UNIT_SIZE;
+        appleY = random.nextInt((int)(SCREEN_HEIGHT / UNIT_SIZE))* UNIT_SIZE;
         
     }
     
@@ -68,6 +70,9 @@ public class GamePanel extends JPanel implements ActionListener{
             g.drawLine(i*UNIT_SIZE, 0, i*UNIT_SIZE, SCREEN_HEIGHT);
             g.drawLine(0, i*UNIT_SIZE, SCREEN_WIDTH, i*UNIT_SIZE);
         }
+        
+        g.setColor(Color.red);
+        g.fillOval(appleX, appleY, UNIT_SIZE, UNIT_SIZE);
     }
     
     public void move(){
