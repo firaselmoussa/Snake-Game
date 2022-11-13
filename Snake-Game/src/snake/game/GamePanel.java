5dafc5712c1a3c21ab16fd493e716a5dfd382bdd
Phoenix-Firas <106271549+Phoenix-Firas@users.noreply.github.com>
2022-11-13 20:32:40 +0200

    implemented gameOver method functionaloity, displayed 'Game Over' & score

--- Snake-Game/src/snake/game/GamePanel.java
+++ Snake-Game/src/snake/game/GamePanel.java
@@ -66,22 +66,31 @@ public class GamePanel extends JPanel implements ActionListener{
     }
     
     public void draw(Graphics g){
-        for(int i = 0; i < SCREEN_HEIGHT/UNIT_SIZE; i++){
-            g.drawLine(i*UNIT_SIZE, 0, i*UNIT_SIZE, SCREEN_HEIGHT);
-            g.drawLine(0, i*UNIT_SIZE, SCREEN_WIDTH, i*UNIT_SIZE);
-        }
         
-        g.setColor(Color.red);
-        g.fillOval(appleX, appleY, UNIT_SIZE, UNIT_SIZE);
-        
-        for(int i = 0; i < bodyParts; i++){
-            if(i == 0){
-                g.setColor(Color.green);
-                g.fillRect(x[i], y[i], UNIT_SIZE, UNIT_SIZE);
-            }else{
-                g.setColor(new Color(45, 180, 0));
-                g.fillRect(x[i], y[i], UNIT_SIZE, UNIT_SIZE);
+        if(running){
+            for(int i = 0; i < SCREEN_HEIGHT/UNIT_SIZE; i++){
+                g.drawLine(i*UNIT_SIZE, 0, i*UNIT_SIZE, SCREEN_HEIGHT);
+                g.drawLine(0, i*UNIT_SIZE, SCREEN_WIDTH, i*UNIT_SIZE);
             }
+
+            g.setColor(Color.red);
+            g.fillOval(appleX, appleY, UNIT_SIZE, UNIT_SIZE);
+
+            for(int i = 0; i < bodyParts; i++){
+                if(i == 0){
+                    g.setColor(Color.green);
+                    g.fillRect(x[i], y[i], UNIT_SIZE, UNIT_SIZE);
+                }else{
+                    g.setColor(new Color(45, 180, 0));
+                    g.fillRect(x[i], y[i], UNIT_SIZE, UNIT_SIZE);
+                }
+            }
+            g.setColor(Color.red);
+            g.setFont(new Font("Ink Free", Font.BOLD, 30));
+            FontMetrics metrics = getFontMetrics(g.getFont());
+            g.drawString("Score: "+applesEaten, (SCREEN_WIDTH - metrics.stringWidth("Game Over"))/2, g.getFont().getSize());
+        }else{
+            gameOver(g);
         }
     }
     
@@ -139,7 +148,15 @@ public class GamePanel extends JPanel implements ActionListener{
     }
     
     public void gameOver(Graphics g){
+        //GAME OVER TEXT
+        g.setColor(Color.red);
+        g.setFont(new Font("Ink Free", Font.BOLD, 75));
+        FontMetrics metrics = getFontMetrics(g.getFont());
+        g.drawString("Game Over", (SCREEN_WIDTH - metrics.stringWidth("Game Over"))/2, SCREEN_HEIGHT/2);
         
+        g.setColor(Color.red);
+        g.setFont(new Font("Ink Free", Font.BOLD, 50));
+        g.drawString("Score: "+applesEaten, (SCREEN_WIDTH - metrics.stringWidth("Score: "+applesEaten))/2, (SCREEN_HEIGHT/2)+(g.getFont().getSize()));
     }
 
     @Override
