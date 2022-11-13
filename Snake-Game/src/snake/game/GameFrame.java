d81d9781109f7094a8ba463ce34e9034ee0c75b4
Phoenix-Firas <106271549+Phoenix-Firas@users.noreply.github.com>
2022-11-13 18:37:26 +0200

    initialised files & classes

---
+++ Snake-Game/src/snake/game/GameFrame.java
@@ -0,0 +1,15 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package snake.game;
+
+import javax.swing.JFrame;
+
+/**
+ *
+ * @author Firas
+ */
+public class GameFrame extends JFrame{
+    
+}
---
+++ Snake-Game/src/snake/game/GamePanel.java
@@ -0,0 +1,23 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package snake.game;
+
+import java.awt.event.ActionEvent;
+import javax.swing.JPanel;
+import java.awt.event.ActionListener;
+
+
+/**
+ *
+ * @author Firas
+ */
+public class GamePanel extends JPanel implements ActionListener{
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+    
+}
